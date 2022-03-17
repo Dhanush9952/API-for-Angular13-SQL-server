65ee527ae1777543843405d8ebbad75b4602eb59
Dhanush <dhanushkrish45@gmail.com>
2022-03-17 08:34:38 +0530

    Added postman API check file

--- WebAPI/Models/FirstAppDempDbContext.cs
+++ WebAPI/Models/FirstAppDempDbContext.cs
@@ -3,13 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using WebAPI.Models; 
 
 
 namespace WebAPI.Models
 {
-    public class FirstAppDempDbContext : DbContext
+    public class FirstAppDempContext : DbContext
     {
+        public FirstAppDempContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public DbSet<Employees> Employees { get; set; }
     }
 
